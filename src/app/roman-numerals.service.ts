edc4ddc0407da9729d983d2e7bd1d435b1c59722
lapak10 <m8r-u6antd@mailinator.com>
2019-05-10 10:40:03 +0530

    1 file modified

--- src/app/roman-numerals.service.ts
+++ src/app/roman-numerals.service.ts
@@ -3,19 +3,22 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RomanNumeralsService {
 
-  integer_array = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  //To define the different Roman numerals. 
+  //We have to add two arrays. One for the Roman numbers, one for or interger.
 
   roman_array = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+  integer_array = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
 
   toRoman(interger_number) {
     let output = '';
 
-
     for (let i = 0; i <= this.integer_array.length; i++) {
 
       while (interger_number % this.integer_array[i] < interger_number) {
-        output += this.roman_array[i];
-        interger_number -= this.integer_array[i];
+        output = output + this.roman_array[i];
+        interger_number = interger_number - this.integer_array[i];
       }
 
     }
@@ -28,8 +31,8 @@ export class RomanNumeralsService {
     for (let i = 0; i <= this.integer_array.length; i++) {
 
       while (roman_string.indexOf(this.roman_array[i]) === 0) {
-        output += this.integer_array[i];
-        roman_string = roman_string.replace(this.roman_array[i], '');
+        output = output +  this.integer_array[i];
+        roman_string = roman_string.replace( this.roman_array[i] , '');
       }
 
     }
